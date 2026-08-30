3916231c10af72dbae4d8aa4cf008b55fc7d01c6
root <root@localhost.localdomain>
2026-08-30 15:08:00 +0000

    fix: lint - add SuppressLint import

--- app/src/main/java/technology/ezequieldevteam/ettoolbox/update/ApkInstaller.kt
+++ app/src/main/java/technology/ezequieldevteam/ettoolbox/update/ApkInstaller.kt
@@ -8,6 +8,7 @@ import android.content.IntentFilter
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
+import androidx.annotation.SuppressLint
 import androidx.core.content.FileProvider
 import technology.ezequieldevteam.ettoolbox.MainActivity
 import java.io.File
